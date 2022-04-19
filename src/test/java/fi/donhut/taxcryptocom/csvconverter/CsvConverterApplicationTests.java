c48fbc49a2e00c42adbb21b11788c0eac076bc1a
Nhut Do <mr.nhut@gmail.com>
2022-04-19 22:58:31 +0300

    Add Spring Boot project structure.

---
+++ src/test/java/fi/donhut/taxcryptocom/csvconverter/CsvConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.donhut.taxcryptocom.csvconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
